4e4c94963bdc4346ad13a5bcc3f9b8ee13ef65a9
Bradley Taylor <qzi@qlik.com>
2025-02-17 00:47:48 -0800

    move projects to json file

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,15 +1,23 @@
-import { glob } from "astro/loaders";
+import { file, glob } from "astro/loaders";
 import { z, defineCollection } from "astro:content";
 
 export const collections = {
   blog: defineCollection({
-    loader: glob({ pattern: "**/[^_]*.md", base: "./src/posts" }),
+    loader: glob({ pattern: "**/[^_]*.md", base: "./src/data/posts" }),
     schema: z.object({
       title: z.string(),
       pubDate: z.date(),
       description: z.string(),
       tags: z.array(z.string()),
       author: z.string(),
+    }),
+  }),
+  projects: defineCollection({
+    loader: file("src/data/projects.json"),
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      link: z.string(),
     }),
   }),
 };

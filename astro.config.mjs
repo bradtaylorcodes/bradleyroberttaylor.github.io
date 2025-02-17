efcd72f5f2b77b421fb40f097cc2737cbb885096
Bradley Taylor <qzi@qlik.com>
2025-02-16 23:15:21 -0800

    add prettierrc and configure new domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://bradleyroberttaylor.github.io',
+  site: 'https://bradtaylor.codes',
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
   markdown: {
     shikiConfig: {
       themes: {
         light: 'github-light-default',
-        dark: 'github-dark-default'
-      }
+        dark: 'github-dark-default',
+      },
     },
   },
 });
+

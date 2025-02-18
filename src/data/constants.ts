be366c1e7e8772f6832d64599ea978dc3c7e9a18
Bradley Taylor <qzi@qlik.com>
2025-02-18 00:25:46 -0800

    extract constants to file

---
+++ src/data/constants.ts
@@ -0,0 +1,4 @@
+export const GITHUB_URL = 'https://github.com/BradleyRobertTaylor';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/bradleyroberttaylor/';
+export const EMAIL = 'taylorbradleyr@gmail.com';
+export const QLIK_AUTOML_URL = 'https://www.qlik.com/us/products/qlik-automl';

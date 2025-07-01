c854ded5141222484bef8d15d2e3e682ca3a1c8a
Bradley Taylor <taylorbradleyr@gmail.com>
2025-06-30 17:05:12 -0700

    update github url and current work

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,4 +1,4 @@
-export const GITHUB_URL = 'https://github.com/BradleyRobertTaylor';
+export const GITHUB_URL = 'https://github.com/bradtaylorcodes';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/bradleyroberttaylor/';
 export const EMAIL = 'taylorbradleyr@gmail.com';
-export const QLIK_AUTOML_URL = 'https://www.qlik.com/us/products/qlik-automl';
+export const ATLASSIAN_URL = 'https://www.atlassian.com/'

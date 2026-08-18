a99baa5cfa48a4fe09f4196b8749819e0e8e7909
BradleyRobertTaylor <taylorbradleyr@gmail.com>
2026-08-17 17:58:57 -0700

    Address portfolio review feedback

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,4 +1,3 @@
 export const GITHUB_URL = 'https://github.com/bradtaylorcodes';
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/bradleyroberttaylor/';
 export const EMAIL = 'taylorbradleyr@gmail.com';
-export const ATLASSIAN_URL = 'https://www.atlassian.com/';
